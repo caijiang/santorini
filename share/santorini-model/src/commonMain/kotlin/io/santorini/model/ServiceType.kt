b89c6a9c5d51e4e870f88e143249d2a58724147a
Cai Jiang <luffy.ja@gmail.com>
2025-12-30 08:34:57 +0800

    more ServiceType

--- share/santorini-model/src/commonMain/kotlin/io/santorini/model/ServiceType.kt
+++ share/santorini-model/src/commonMain/kotlin/io/santorini/model/ServiceType.kt
@@ -6,7 +6,18 @@ import kotlin.js.JsExport
  *
  * @author CJ
  */
+@Suppress("unused")
 @JsExport
 enum class ServiceType {
-    JVM
+    JVM,
+
+    /**
+     * 前端服务
+     */
+    Nginx,
+
+    /**
+     * 没有具体的业务逻辑
+     */
+    Other,
 }
